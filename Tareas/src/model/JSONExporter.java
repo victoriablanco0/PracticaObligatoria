5ecde6e261f2b854b0c11d2fde90022405070d66
victoriablanco0 <victoriablanco@usal.es>
2024-12-10 17:48:58 +0100

    importar json

--- Tareas/src/model/JSONExporter.java
+++ Tareas/src/model/JSONExporter.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 
 public class JSONExporter implements IExporter{
@@ -24,23 +25,21 @@ Path ruta = Paths.get(System.getProperty("user.home"), "Downloads", "output.json
 
 @Override
     public List<Task> importarTareas() {
-        ArrayList<Task> tareas = new ArrayList<>();
-        Gson gson = new Gson();
+           List<Task> tareas = new ArrayList<>();  // Inicializamos la lista de tareas
+    Gson gson = new GsonBuilder()
+            .registerTypeAdapter(Date.class, new DateAdapter())  // Registrar el TypeAdapter para Date
+            .create();
 
+    String filePath = System.getProperty("user.home") + File.separator + "Downloads" + File.separator + "output.json";
 
-        try (FileReader reader = new FileReader("output.json")){
-            List<String> lineas = Files.readAllLines(ruta);
-            for(String linea:lineas){
-                Task tarea = gson.fromJson(linea, Task.class);
-                if(tarea!=null){
-                    tareas.add(tarea);
-                }
-
-            }
-            return tareas;
-        } catch (IOException e) {
-            return null;
-        }
+    try (FileReader reader = new FileReader(filePath)) {
+        // Leemos todo el archivo JSON y lo convertimos a una lista de objetos Task
+        tareas = gson.fromJson(reader, new TypeToken<List<Task>>(){}.getType());
+        return tareas;  // Devolvemos la lista de tareas deserializadas
+    } catch (IOException e) {
+        System.err.println("Error al importar tareas: " + e.getMessage());
+        return null;  // Si ocurre un error, devolvemos null
+    } 
     }
 
 
